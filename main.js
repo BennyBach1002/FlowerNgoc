37f508b20db77bb450c53b855dad6f81ddee0a67
BennyBach1002 <benbach2001@gmail.com>
2025-10-22 01:01:52 +0700

    5th commit

--- main.js
+++ main.js
@@ -1,6 +1,32 @@
 onload = () => {
-    const c = setTimeout(() => {
-      document.body.classList.remove("not-loaded");
-      clearTimeout(c);
-    }, 1000);
-  };
+  const c = setTimeout(() => {
+    document.body.classList.remove("not-loaded");
+    clearTimeout(c);
+  }, 1000);
+};
+
+function updateText(text) {
+  let delay = 200;
+
+  let h1 = document.getElementById("animated");
+
+  h1.innerHTML = text
+    .split("")
+    .map((letter) => {
+      console.log(letter);
+      return `<span>` + letter + `</span>`;
+    })
+    .join("");
+
+  Array.from(h1.children).forEach((span, index) => {
+    setTimeout(() => {
+      span.classList.add("wavy");
+    }, index * 60 + delay);
+  });
+}
+
+document.getElementById("textField").addEventListener("input", (event) => {
+  updateText(event.target.value);
+});
+
+updateText("Ngọc Happy 20-10");
